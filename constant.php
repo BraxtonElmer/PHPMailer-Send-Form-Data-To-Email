f65b5699c7b9e08c24039a516d82e4f1236291f9
BraxtonElmer <65809818+BraxtonElmer@users.noreply.github.com>
2020-10-01 15:08:32 +0530

    Update constant.php

--- constant.php
+++ constant.php
@@ -1,6 +1,6 @@
 <?php
 
-// SMTP Configuration
+// SMTP Configuration for sending Form Data
 define('HOST',"ssl://smtp.gmail.com"); // Host is ssl://smtp.gmail.com
 define('USERNAME',"sender.of.form.data@gmail.com"); // gmail address from which mail must be sent
 define('PASSWORD',"password-of-sender"); // password of gmail from which mail must be sent
@@ -8,4 +8,5 @@ define('PASSWORD',"password-of-sender"); // password of gmail from which mail mu
 // Recipient Configuration
 define('RECIPIENT_MAIL',"recieving.mail.address@gmail.com"); // email address to where the email must be recieved
 define('RECIPIENT_MAIL_NAME',"name"); // name of the sender, you can name it as website, not compulsary
+
 ?>
